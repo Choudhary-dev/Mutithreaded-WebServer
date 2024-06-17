b80c9c35125ea030de4190bc0aa34c9db9a75236
Chandan Choudhary <70895174+Chandan-0320@users.noreply.github.com>
2024-06-17 20:52:56 +0530

    Multithreaded webserver

---
+++ Client.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    
+
+    public void run() throws UnknownHostException,IOException{
+
+        int port =8010;
+
+        
+            InetAddress idr =InetAddress.getByName("localhost");
+
+            Socket socket= new Socket(idr,port);
+
+            PrintWriter toSocket=new PrintWriter(socket.getOutputStream());
+            BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            toSocket.println("Heloo from the client");
+
+            String line= fromSocket.readLine();
+
+            System.out.println("response from the  socket"+line);
+
+            toSocket.close();
+            fromSocket.close();
+            socket.close();
+
+        
+
+
+        
+
+    }
+
+    public static void main(String[] args) {
+
+        try{
+            Client client = new Client();
+            client.run();
+
+        }
+        catch(Exception e){
+            
+        }
+        
+    }
+}
